2965a9d2bc65e86193e3d1a627875d73348d4540
Kirsten <kirsten.netter@gmx.de>
2022-08-18 15:33:14 +0200

    generated component welcome-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,15 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
